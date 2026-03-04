e50b4e1a004e26b2dba6d3d1c92585aed04cd20a
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2026-03-04 19:55:30 +0800

    task 4 example

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,41 @@ server.use(express.json());
 //   console.log("okey");
 // });
 
+
+// Params бодлогууд (req.params)
+// 1. /hello/:name зам үүсгэ. name-ийг уншаад "Сайн байна уу, [нэр]!" гэж буцаа.
+
+server.get("/hello/:name", (req: Request, res: Response)=>
+{
+  res.status(200).send(`sain bna uu? ${req.params.name}`)
+
+  console.log(`sain bna uu? ${req.params.name}`)
+})
+
+// 2. /age/:age зам үүсгэ. age-ийг уншаад "Чи [нас] настай байна" гэж буцаа.
+
+server.get("/age/:age", (req: Request, res: Response)=>
+{
+  res.status(200).send(`Чи ${req.params.age} настай байна`)
+
+  console.log(`Чи ${req.params.age} настай байна`)
+})
+
+// 3. /add/:a/:b зам үүсгэ. Хоёр тоог нэмээд үр дүнг буцаа. (Санамж: params бол string байдаг, тоо болгохын тулд Number() ашигла)
+
+server.get("/add/:a/:b",(req: Request, res: Response)=>
+{
+
+const a= Number(req.params.a)
+const b= Number(req.params.b)
+const c=a+b;
+
+
+  res.status(200).send(`Niilber ni: ${c}= ${a}+${b}`)
+
+  console.log(`Niilber ni: ${c} = ${a}+${b}`)
+})
+
 // 2-р даалгавар: Query (Хайлтын мөр)
 // Тайлбар: URL-ийн ард ? тэмдгээр зааглагдаж ирдэг нэмэлт өгөгдөл.
 // Даалгавар: /filter зам үүсгэж, city болон age гэсэн query-г уншиж аваад JSON хэлбэрээр буцаа.
@@ -30,15 +65,85 @@ server.use(express.json());
 //   console.log("okey");
 // });
 
+// Query бодлогууд (req.query)
+// 4. /greet?name=Bat — name query-г уншаад "Сайн байна уу, Bat!" гэж буцаа.
+
+server.get("/greet", (req: Request, res: Response)=>
+{
+  const {name} =req.query;
+
+  res.status(200).send(`sain bna uu?, ${name}`)
+
+  console.log(`sain bna uu?, ${name}`)
+})
+
+// 5. /multiply?a=3&b=4 — хоёр тоог үржүүлээд буцаа.
+
+server.get("/multiply",(req: Request, res: Response)=>
+{
+  const a =Number(req.query.a);
+  const b =Number(req.query.b);
+  const c= a*b;
+
+  res.status(200).send(`Hoyr toonii urjwer ni: ${c}`)
+
+  console.log(`Hoyr toonii urjwer ni: ${c}`)
+})
+
+// 6. /search?keyword=apple — "Та [keyword]-г хайлаа" гэж буцаа.
+
+server.get("/search", (req:Request, res: Response)=>
+{
+  const {keyword}= req.query;
+
+  res.status(200).send(`Ta ${keyword}-g hailaa`)
+
+  console.log(`Ta ${keyword}-g hailaa`)
+})
+
+// Headers бодлогууд (req.headers)
+// 7. /whoami зам үүсгэ. user-agent header-ийг консолд хэвлэ.
+
+server.get("/whoami", (req: Request, res: Response)=>
+{
+  const userAgent= req.headers["user-agent"];
+
+  res.status(200).send(`${userAgent}`)
+
+  console.log(`${userAgent}`)
+})
+
+// 8. /secret зам үүсгэ. auth-token header байвал "Тавтай морил!", байхгүй бол "Токен байхгүй!" гэж буцаа.
+
+server.get("/secret", (req: Request, res: Response)=>
+{
+const authToken= req.headers["auth-token"]
+
+if(authToken)
+{
+  console.log(`Tawtai moril`)
+  return  res.status(200).send(`Tawtai moril`)
+}
+else {
+  console.log(`Token bhgui`)
+  return res.status(404).send(`Token bhgui`)
+}
+
+  
+})
+
 // 3-р даалгавар: Headers (Толгой мэдээлэл)
 // Тайлбар: Хэрэглэгчийн харагдах орчинд биш, хүсэлтийн далд мета өгөгдөлд байдаг мэдээлэл.
 // Даалгавар: Хүсэлтийн header-ээс user-agent (хөтөч) болон auth-token гэсэн мэдээллийг авч консол дээр хэвлэ.
 // Турших хэрэгсэл: Postman эсвэл Hoppscotch ашиглан Header хэсэгт утга өгч шалгана.
 
 server.get("/headers", (req: Request, res: Response) => {
-  res.status(200).send("ok");
+const userAgent= req.headers["user-agent"]
+const authToken= req.headers["auth-token"]
 
-  console.log("okey");
+  res.status(200).send(`${userAgent} bas ${authToken}`);
+
+  console.log(`${userAgent} bas ${authToken}`);
 });
 
 server.listen(port, () => {
